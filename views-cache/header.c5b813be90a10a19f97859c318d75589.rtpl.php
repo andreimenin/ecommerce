bd78452a0fac0c171ad566f92dd0c6df7948a39a
Andrei Menin <andreimenin@gmail.com>
2018-05-08 16:37:25 -0300

    criamos a classe Page

---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,12 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+
+<!-- HEADER DO SISTEMA -->
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Hcode Store</title>
+	</head>
+
+	<body>			
+		
